b6a54db3a8b22411b7064e6d84abc8cd743815bd
Robert Bennett <rltbennett@icloud.com>
2021-12-01 12:02:05 -0500

    Did day 1

---
+++ Day 1/aoc_01_1.jl
@@ -0,0 +1,6 @@
+module Aoc01_02
+include("aoc_01_setup.jl")
+using .Aoc01: get_n_increasing_running_avg_depths
+end
+
+Aoc01_02.get_n_increasing_running_avg_depths(3)
---
+++ Day 1/aoc_01_2.jl
@@ -0,0 +1,6 @@
+module Aoc01_01
+include("aoc_01_setup.jl")
+using .Aoc01: get_n_increasing_running_avg_depths
+end
+
+Aoc01_01.get_n_increasing_running_avg_depths(1)
