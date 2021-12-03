a97f887153b851d265a1555a9007df470c239d73
Robert Bennett <rltbennett@icloud.com>
2021-12-03 10:35:38 -0500

    Added Iosevka fonts Did day 2 Moved days into src/ Consolidated files

---
+++ src/day_02/soln.jl
@@ -0,0 +1,43 @@
+# tag::pt1[]
+horizontal = vertical = 0
+for line in eachline(joinpath(@__DIR__, "input.txt"))
+    dir, dist = line |> strip |> split
+    dist = parse(Int, dist)
+
+    if dir == "forward"
+        horizontal += dist
+    elseif dir == "up"
+        vertical -= dist
+    elseif dir == "down"
+        vertical += dist
+    else
+        throw("Invalid direction $(dir)")
+    end
+
+end
+
+ans = horizontal * vertical
+@show ans
+# end::pt1[]
+
+# tag::pt2[]
+horizontal = vertical = aim = 0
+for line in eachline(joinpath(@__DIR__, "input.txt"))
+    dir, dist = line |> strip |> split
+    dist = parse(Int, dist)
+
+    if dir == "forward"
+        horizontal += dist
+        vertical += aim * dist
+    elseif dir == "up"
+        aim -= dist
+    elseif dir == "down"
+        aim += dist
+    else
+        throw("Invalid direction $(dir)")
+    end
+end
+
+ans = horizontal * vertical
+@show ans
+# end::pt2[]
